be43871490a4837387b85f3e9058fbae574cf56d
Filip Tokarski <filip.tokarski14@gmail.com>
2019-05-29 14:11:17 +0200

    Updated requirements, adding tests

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+[![Coverage Status](https://coveralls.io/repos/github/FilipTokarski/url_shortener2/badge.svg?branch=master)](https://coveralls.io/github/FilipTokarski/url_shortener2?branch=master)  
 # url_shortener2  
 New version of my url shortener app  
 
--- url_shortener/shortener/tests.py
+++ url_shortener/shortener/tests.py
@@ -1,5 +1,9 @@
-from django.test import TestCase
+#from django import status_code
+from django.test import Client, TestCase
+from django.urls import reverse
+
 from .models import UrlModel
+from .utils import generate_random_string
 
 
 class TestModel(TestCase):
@@ -7,8 +11,37 @@ class TestModel(TestCase):
     def setUp(self):
         UrlModel.objects.create(original_url='https://www.google.com/')
 
+    def test_generate_random_string(self):
+        data = generate_random_string()
+
+        self.assertEqual(type(data), str)
+        self.assertTrue(data.isalnum())
+        self.assertEqual(len(data), 8)
+
     def test_model(self):
         url = UrlModel.objects.get(id=1)
 
         self.assertIsNotNone(UrlModel.objects.all())
+        self.assertIsNotNone(url.url_slug)
         self.assertEqual(url.original_url, 'https://www.google.com/')
+        self.assertTrue(url.shortened_url.startswith('https://ftdevprojects.pl/'))
+        self.assertTrue(url.date_added)
+
+
+class TestViews(TestCase):
+
+    def test_get_create_url_view(self):
+        client = Client()
+        response = client.get(reverse('create-url'))
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_create_url_view(self):
+        client = Client()
+        response = client.post(reverse('create-url'),
+                                 {'original_url': 'https://www.google.com/'})
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_redirect_view(self):
+        pass
