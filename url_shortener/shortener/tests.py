43eaff2bee23d5ea81364ac754281e1e210f6bcd
Filip Tokarski <filip.tokarski14@gmail.com>
2019-05-29 13:40:03 +0200

    Minor changes, adding tests

--- url_shortener/shortener/tests.py
+++ url_shortener/shortener/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import UrlModel
 
-# Create your tests here.
+
+class TestModel(TestCase):
+
+    def setUp(self):
+        UrlModel.objects.create(original_url='https://www.google.com/')
+
+    def test_model(self):
+        url = UrlModel.objects.get(id=1)
+
+        self.assertIsNotNone(UrlModel.objects.all())
+        self.assertEqual(url.original_url, 'https://www.google.com/')
--- url_shortener/url_shortener/settings.py
+++ url_shortener/url_shortener/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '' # Your secret key here
+SECRET_KEY = 'gf84f837fg8378f7^$%R#*&GD*Dig38g8dg^F^&*G'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['ftdevprojects.pl']
+ALLOWED_HOSTS = []
 
 
 # Application definition
